17587ef471802dc5978ded026fa490843312caed
weiiwu <master_wu51@hotmail.com>
2025-05-14 17:25:08 +1000

    First commit

---
+++ config/inngest.js
@@ -0,0 +1,60 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "cartApp" });
+
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk'
+    },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            eamil: email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_url: image_url
+        }
+        await connectDB()
+        await User.create(userData)
+
+    }
+)
+
+// Inngest fun to update user data in DB
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.updated'
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            eamil: email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_url: image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id,userData)
+    }
+)
+
+// Inngest fun to delete user data in DB
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+    },
+    {event: 'clerk/user.deleted'},
+    async({event}) => {
+        const {id} = event.data
+
+        await connectDB()
+        await User.findByIdAndDelete(id)
+    }
+)
