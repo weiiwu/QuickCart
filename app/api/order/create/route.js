fdd95894ee3090707158d561b86ffeff53b15079
weiiwu <master_wu51@hotmail.com>
2025-06-24 16:04:02 +1000

    Order Summary

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
         // calcute amount using items
         const amount = await items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return acc + product.offerPrice * item.quantity;
+            return await acc + product.offerPrice * item.quantity;
         }, 0)
 
         await inngest.send({
--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -35,6 +35,38 @@ const OrderSummary = () => {
   };
 
   const createOrder = async () => {
+    try {
+      if (!selectedAddress) {
+        return toast.error('Please select an address')
+      }
+
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({ product: key, quantity: cartItems[key] }))
+      cartItemsArray = cartItemsArray.filter(item => item.quantity > 0)
+
+      if (cartItemsArray.length === 0) {
+        return toast.error('Cart is empty')
+      }
+
+      const token = await getToken()
+
+      const { data } = await axios.post('/api/order/create', {
+        address: selectedAddress._id,
+        items: cartItemsArray
+      }, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+
+      if (data.success) {
+        toast.success(data.message)
+        setCartItems({})
+        router.push('/order-placed')
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
 
   }
 
